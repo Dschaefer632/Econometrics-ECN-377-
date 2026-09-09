527060e08a09e3dae4843981a556b2e4b46aa6f1
Dschaefer632 <des6179@uncw.edu>
2026-09-09 09:17:00 -0400

    Add files via upload

---
+++ Quiz 3 R Cheat sheet.R
@@ -0,0 +1,32 @@
+#The SAMPLE variance divides the sum of squared deviations from the mean by: n-1
+#Which R command computes the SAMPLE variance of a numeric vector x? var(x)
+#Which R command computes the correlation between numeric vectors x and y? cor(x, y) 
+#The correlation between two variables is always: between -1 and 1
+#For a discrete random variable X the expected value E[X] equals The sum of each value times its probability 
+#For a discrete random variable X E[X^2] equals The sum of each SQUARED value times its probability 
+#True or False: in R, sd(x) gives the SAMPLE standard deviation TRUE
+#Sample Mean:
+#x <- c(9, 9, 1)
+#mean(x)
+#Sample Variance 
+#x <- c(9, 9, 1)
+#var(x)
+#SD
+#x <- c(9, 9, 1)
+#sd(x)
+#Covariance 
+#x <- c(1, 0, 2)
+#y <- c(1, 0, 5)
+#cov(x, y)
+#Correlation
+#x <- c(6, 1, 1)
+#y <- c(2, 7, 5)
+#cor(x, y)
+#Probability Questions
+#x <- c(2, 0)
+#p <- c(0.5, 0.5)
+#sum(x * p)
+#If the E[X^2]
+#x <- c(2, 0)
+#p <- c(0.5, 0.5)
+#sum(x^2 * p)
